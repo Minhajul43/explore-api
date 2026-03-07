d819298f730de9225a885abc8d639dccd13f895b
Minhajul43 <minhajul255685islam@gmail.com>
2026-03-08 00:04:19 +0600

    appendChild postContainer

--- posts.js
+++ posts.js
@@ -9,6 +9,28 @@ const LoadPost = () => {
 };
 
 const displayPost = (posts) => {
+  // get the container
+  const postContainer = document.getElementById('post-container');
+  postContainer.innerHTML = "";// Create empty in previous data and load new data
+
+  // console.log(post-container);
+
+
+  posts.forEach(post => {
+    console.log(post.title);
+
+    //2.create html element
+
+    const li = document.createElement('li');
+    li.innerText = post.title;
+    // console.log(li);
+
+    // 3.append child
+    postContainer.appendChild(li);
+    console.log(postContainer);
+  
+  });
+  
   // console.log(posts);
   // for (let i = 0; i < posts.length; i++) {
   //   console.log(posts[i]);
@@ -18,7 +40,7 @@ const displayPost = (posts) => {
   //   console.log(post);
   // }
 
-  posts.forEach(post => {
-    console.log(post);
-  })
+  // posts.forEach(post => {
+  //   console.log(post);
+  // })
 };

a1f0326b34267634bf5b0639ef30d44e2cd6696f
Minhajul43 <minhajul255685islam@gmail.com>
2026-03-08 22:40:59 +0600

    .

--- posts.js
+++ posts.js
@@ -8,39 +8,41 @@ const LoadPost = () => {
     })
 };
 
-const displayPost = (posts) => {
-  // get the container
-  const postContainer = document.getElementById('post-container');
-  postContainer.innerHTML = "";// Create empty in previous data and load new data
-
-  // console.log(post-container);
-
-
-  posts.forEach(post => {
-    console.log(post.title);
-
-    //2.create html element
-
-    const li = document.createElement('li');
-    li.innerText = post.title;
-    // console.log(li);
-
-    // 3.append child
-    postContainer.appendChild(li);
-    console.log(postContainer);
+// const displayPost = (posts) => {
   
-  });
+//   // get the container
+//   const postContainer = document.getElementById('post-container');
+//   postContainer.innerHTML = "";// Create empty in previous data and load new data
+
+//   // console.log(post-container);
+
+
+//   posts.forEach(post => {
+//     console.log(post.title);
+
+//     //2.create html element
+
+//     const li = document.createElement('li');
+//     li.innerText = post.title;
+//     // console.log(li);
+
+//     // 3.append child
+//     postContainer.appendChild(li);
+//     console.log(postContainer);
   
-  // console.log(posts);
-  // for (let i = 0; i < posts.length; i++) {
-  //   console.log(posts[i]);
-  // }
+//   });
+  
+//   // console.log(posts);
+//   // for (let i = 0; i < posts.length; i++) {
+//   //   console.log(posts[i]);
+//   // }
 
-  // for (const post of posts) {
-  //   console.log(post);
-  // }
+//   // for (const post of posts) {
+//   //   console.log(post);
+//   // }
 
-  // posts.forEach(post => {
-  //   console.log(post);
-  // })
-};
+//   // posts.forEach(post => {
+//   //   console.log(post);
+//   // })
+// };
+
